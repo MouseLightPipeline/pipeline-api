7f8a00331e5817704195f7120877aaa51d028f8c
Patrick Edson <pedson@leapscientific.com>
2017-01-09 09:47:53 -0500

    Task update from client

--- seeds/basic_seed.js
+++ seeds/basic_seed.js
@@ -16,6 +16,16 @@ function productionSeed() {
                 return Promise.all([
                     // Inserts seed entries
                     knex('TaskDefinition').insert({
+                        id: '1161F8E6-29D5-44B0-B6A9-8D3E54D23292'.toLocaleLowerCase(),
+                        name: 'ilastik Axon UInt16',
+                        description: 'ilastik Axon UInt16 hard-coded to ',
+                        script: '/groups/mousebrainmicro/mousebrainmicro/erhan_dm11/AxonClassifier',
+                        interpreter: 'none',
+                        args: '/Users/pedson/Developer/Leap/Janelia/acq-dashboard-worker-api/test/pixel_classifier_test',
+                        work_units: 10,
+                        created_at: createDate(3, 0)
+                    }),
+                    knex('TaskDefinition').insert({
                         id: '1EC76026-4ECC-4D25-9C6E-CDF992A05DA3'.toLocaleLowerCase(),
                         name: 'ilastik Pixel Classifier Test',
                         description: 'Calls ilastik with test project.',
@@ -71,6 +81,16 @@ function developmentSeed() {
             .then(() => {
                 return Promise.all([
                     // Inserts seed entries
+                    knex('TaskDefinition').insert({
+                        id: '1161F8E6-29D5-44B0-B6A9-8D3E54D23292'.toLocaleLowerCase(),
+                        name: 'ilastik Axon UInt16',
+                        description: 'ilastik Axon UInt16 hard-coded to ',
+                        script: '/groups/mousebrainmicro/mousebrainmicro/erhan_dm11/AxonClassifier',
+                        interpreter: 'none',
+                        args: '/Users/pedson/Developer/Leap/Janelia/acq-dashboard-worker-api/test/pixel_classifier_test',
+                        work_units: 10,
+                        created_at: createDate(3, 0)
+                    }),
                     knex('TaskDefinition').insert({
                         id: '1EC76026-4ECC-4D25-9C6E-CDF992A05DA3'.toLocaleLowerCase(),
                         name: 'ilastik Pixel Classifier Test',
