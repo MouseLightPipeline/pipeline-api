cac971a572a5d42df18f642035a7774263d03535
Patrick Edson <pedson@users.noreply.github.com>
2018-03-19 16:55:46 -0400

    Update test project configuration.

--- sequelize-seeds/20170707115836-initialize.ts
+++ sequelize-seeds/20170707115836-initialize.ts
@@ -59,6 +59,25 @@ function createTaskRepositories(when: Date) {
 function createTaskDefinitions(when: Date) {
     if (isProduction) {
         return [{
+            id: "ae111b6e-2187-4e07-8ccf-bc7d425d95af",
+            name: "Line Fix",
+            description: "",
+            script: "lineFix.sh",
+            interpreter: "none",
+            script_args: JSON.stringify({
+                arguments: [{
+                    value: "/groups/mousebrainmicro/mousebrainmicro/Software/pipeline/apps/lineFix",
+                    type: TaskArgumentType.Literal
+                }]
+            }),
+            cluster_args: JSON.stringify({arguments: ["-n 4 -R\"affinity[core(1)]\""]}),
+            expected_exit_code: 0,
+            work_units: 4,
+            cluster_work_units: 1,
+            log_prefix: "lf",
+            task_repository_id: "04dbaad7-9e59-4d9e-b7b7-ae3cd1248ef9",
+            created_at: when
+        }, {
             id: "1161f8e6-29d5-44b0-b6a9-8d3e54d23292",
             name: "Axon UInt16",
             description: "Axon UInt16",
@@ -154,25 +173,6 @@ function createTaskDefinitions(when: Date) {
             log_prefix: "pm",
             task_repository_id: "04dbaad7-9e59-4d9e-b7b7-ae3cd1248ef9",
             created_at: when
-        }, {
-            id: "ae111b6e-2187-4e07-8ccf-bc7d425d95af",
-            name: "Line Fix",
-            description: "",
-            script: "lineFix.sh",
-            interpreter: "none",
-            script_args: JSON.stringify({
-                arguments: [{
-                    value: "/groups/mousebrainmicro/mousebrainmicro/Software/pipeline/apps/lineFix",
-                    type: TaskArgumentType.Literal
-                }]
-            }),
-            cluster_args: JSON.stringify({arguments: ["-n 4 -R\"affinity[core(1)]\""]}),
-            expected_exit_code: 0,
-            work_units: 4,
-            cluster_work_units: 1,
-            log_prefix: "lf",
-            task_repository_id: "04dbaad7-9e59-4d9e-b7b7-ae3cd1248ef9",
-            created_at: when
         }];
     } else {
         return [{
@@ -193,6 +193,25 @@ function createTaskDefinitions(when: Date) {
             cluster_work_units: 1,
             log_prefix: "ec",
             task_repository_id: "f22c6e43-782c-4e0e-b0ca-b34fcec3340a",
+            created_at: when
+        }, {
+            id: "ae111b6e-2187-4e07-8ccf-bc7d425d95af",
+            name: "Line Fix",
+            description: "",
+            script: "lineFix.sh",
+            interpreter: "none",
+            script_args: JSON.stringify({
+                arguments: [{
+                    value: "/groups/mousebrainmicro/mousebrainmicro/Software/pipeline/apps/lineFix",
+                    type: TaskArgumentType.Literal
+                }]
+            }),
+            cluster_args: JSON.stringify({arguments: ["-n 4 -R\"affinity[core(1)]\""]}),
+            expected_exit_code: 0,
+            work_units: 4,
+            cluster_work_units: 1,
+            log_prefix: "lf",
+            task_repository_id: "04dbaad7-9e59-4d9e-b7b7-ae3cd1248ef9",
             created_at: when
         }, {
             id: "1ec76026-4ecc-4d25-9c6e-cdf992a05da3",
@@ -326,37 +345,49 @@ function createProjects(when: Date) {
 function createPipelineStages(when: Date) {
     if (isProduction) {
         return [{
-            id: "828276a5-44c0-4bd1-87f7-9495bc3e9f6c",
-            name: "Classifier",
-            description: "Classifier",
-            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_1_classifier_output",
+            id: "90e86015-65c9-44b9-926d-deaced40ddaa",
+            name: "Line Fix",
+            description: "Line Fix",
+            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_1_line_fix_output",
             function_type: 2,
             is_processing: false,
             depth: 1,
             project_id: "44e49773-1c19-494b-b283-54466b94b70f",
-            task_id: "1161f8e6-29d5-44b0-b6a9-8d3e54d23292",
+            task_id: "ae111b6e-2187-4e07-8ccf-bc7d425d95af",
             previous_stage_id: null,
+            created_at: when
+        }, {
+            id: "828276a5-44c0-4bd1-87f7-9495bc3e9f6c",
+            name: "Classifier",
+            description: "Classifier",
+            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_2_classifier_output",
+            function_type: 2,
+            is_processing: false,
+            depth: 2,
+            project_id: "44e49773-1c19-494b-b283-54466b94b70f",
+            task_id: "1161f8e6-29d5-44b0-b6a9-8d3e54d23292",
+            previous_stage_id: "90e86015-65c9-44b9-926d-deaced40ddaa",
             created_at: when
         }, {
             id: "5188b927-4c50-4f97-b22b-b123da78dad6",
             name: "Descriptors",
             description: "Descriptors",
-            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_2_descriptor_output",
+            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_3_descriptor_output",
             function_type: 2,
             is_processing: false,
-            depth: 2,
+            depth: 3,
             project_id: "44e49773-1c19-494b-b283-54466b94b70f",
             task_id: "a9f21399-07c0-425c-86f6-6e4f45bb06b9",
             previous_stage_id: "828276a5-44c0-4bd1-87f7-9495bc3e9f6c",
             created_at: when
         }, {
             id: "2683ad99-e389-41fd-a54c-38834ccc7ae9",
-            name: "Merge Descriptors",
-            description: "Descriptor Merge",
-            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_3_descriptor_merge",
-            function_type: 2,
+            name: "Point Match",
+            description: "Point Match",
+            dst_path: "/nrs/mouselight/pipeline_output/2016-10-31-jan-demo/stage_4_point_match_output",
+            function_type: 5,
             is_processing: false,
-            depth: 3,
+            depth: 4,
             project_id: "44e49773-1c19-494b-b283-54466b94b70f",
             task_id: "3ba41d1c-13d0-4def-9b5b-54d940a0fa08",
             previous_stage_id: "5188b927-4c50-4f97-b22b-b123da78dad6",
@@ -392,7 +423,7 @@ function createPipelineStages(when: Date) {
             name: "Merge Descriptors",
             description: "Descriptor Merge",
             dst_path: "/Volumes/Spare/Projects/MouseLight/PipelineOutput3",
-            function_type: 2,
+            function_type: 5,
             is_processing: false,
             depth: 3,
             project_id: "af8cb0d4-56c0-4db8-8a1b-7b39540b2d04",
@@ -414,6 +445,14 @@ function createPipelineStageFunction(when: Date) {
         created_at: when
     }, {
         id: 3,
+        name: "Map With X Index - 1 Tile",
+        created_at: when
+    }, {
+        id: 4,
+        name: "Map With Y Index - 1 Tile",
+        created_at: when
+    }, {
+        id: 5,
         name: "Map With Z Index - 1 Tile",
         created_at: when
     }];
